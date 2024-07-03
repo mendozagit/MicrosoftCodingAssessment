5b963069620526dc8830569cad6f1dad4e8ee3e4
JesusMendoza <mendoza.git@gmail.com>
2024-07-03 13:37:02 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MicrosoftCodingAssessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = SmallInt([-1, -3]);
+
+            Console.WriteLine("Hello, World!"+ result);
+        }
+
+        static int SmallInt(int[] A)
+        {
+            HashSet<int> positives = new HashSet<int>();
+
+            foreach (var num in A)
+            {
+                if (num>0)
+                {
+                    positives.Add(num);
+                }
+            }
+
+            int result = 1;
+            while (positives.Contains(result))
+            {
+                result++;
+            }
+            return result;
+
+            //Array.Sort(A);
+
+            // get next positive number that is not in the array
+            //for (int i = 1; i < nums.Length; i++)
+            //{
+            //    if (nums[i] - nums[i - 1] > 1)
+            //    {
+            //        return nums[i - 1] + 1;
+            //    }
+            //    else
+            //    {
+            //        //return first next positive number that is not in the array
+            //        return 1;
+            //    }
+            //}
+
+            //return nums[nums.Length]+1;
+        }
+    }
+}
